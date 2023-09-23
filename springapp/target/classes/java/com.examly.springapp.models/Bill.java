7645229d7794d01f053db1cedc79a1b24cb7c354
Iamneo <techadmin@iamneo.ai>
2023-09-23 07:46:34 +0000

    Backup Commit

---
+++ springapp/target/classes/java/com.examly.springapp.models/Bill.java
@@ -0,0 +1,40 @@
+package com.examly.springapp;
+
+import java.lang.annotation.Inherited;
+
+@Entity
+@Table(name="BILLS")
+public class Bill{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="BILLID")
+    private int billId;
+    @Column(name="TOTAL")
+    private int total;
+    public int getBillId() {
+        return billId;
+    }
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+    public int getTotal() {
+        return total;
+    }
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    public Bill(int billId, int total) {
+        this.billId = billId;
+        this.total = total;
+    }
+    public Bill() {
+        super();
+    }
+    @Override
+    public String toString() {
+        return "Bill [billId=" + billId + ", total=" + total + "]";
+    }
+
+
+}
