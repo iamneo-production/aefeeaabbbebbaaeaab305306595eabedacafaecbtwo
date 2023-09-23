7b5b622056e8f40915bd14af57e23cd6b60a9473
Iamneo <techadmin@iamneo.ai>
2023-09-23 07:11:34 +0000

    Backup Commit

--- springapp/src/main/resources/java/java.com.examly.springapp.models/Bill.java
+++ springapp/src/main/resources/java/java.com.examly.springapp.models/Bill.java
@@ -1,3 +1,28 @@
+package com.examly.springapp;
+
 public class Bill{
+
+    private int billId;
+    private int total;
+    public int getBillId() {
+        return billId;
+    }
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+    public int getTotal() {
+        return total;
+    }
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    public Bill(int billId, int total) {
+        this.billId = billId;
+        this.total = total;
+    }
+    public Bill() {
+        super();
+    }
+
 
 }
--- springapp/src/main/resources/java/java.com.examly.springapp.models/Customer.java
+++ springapp/src/main/resources/java/java.com.examly.springapp.models/Customer.java
@@ -1,8 +1,51 @@
+package com.examly.springapp;
 public class Customer {
     private int customerId;
     private String customerName;
     private int age;
     private long mobile;
     bill Bill;
-     
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public long getMobile() {
+        return mobile;
+    }
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+    public bill getBill() {
+        return Bill;
+    }
+    public void setBill(bill bill) {
+        Bill = bill;
+    }
+    public Customer(int customerId, String customerName, int age, long mobile, bill bill) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.age = age;
+        this.mobile = mobile;
+        Bill = bill;
+    }
+    public Customer() {
+        super
+    }
+
+    
+
 }
