2d84eca1cb57f88bed67508b624e5a6973ec62de
Iamneo <techadmin@iamneo.ai>
2023-09-23 07:41:34 +0000

    Backup Commit

--- springapp/src/main/resources/java/com.examly.springapp.controllers/ApiController.java
+++ springapp/src/main/resources/java/com.examly.springapp.controllers/ApiController.java
@@ -1,7 +1,20 @@
 package com.examly.springapp.controllers;
 
+@RestController
 public class ApiController {
 
-    
+    @GetMapping("/employee/{id}")
+	public Optional<Employee> getEmpDetails(@PathVariable(value = "id") int empId) {
+
+		EmpDTO response = null;
+
+		System.out.println("inside controller " + empId);
+		Optional<Employee> employee = employeeService.findEmpById(empId);
+		if (employee != null) {
+			return employee;
+		} else {
+			return null;
+		}
+	}
     
 }
